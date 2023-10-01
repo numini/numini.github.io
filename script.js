34c63176ba20721d2241bd1b7cf842958f7a5a38
numini <143790958+numini@users.noreply.github.com>
2023-10-01 09:30:46 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+function search() {
+  const searchTerm = document.getElementById('searchInput').value.toLowerCase().trim();
+
+  // Cek apakah kotak pencarian kosong
+  if (searchTerm === '') {
+    alert('Masukkan kata kunci pencarian.');
+    return;  // Berhenti jika kotak pencarian kosong
+  }
+
+  const posts = document.querySelectorAll('.post');
+
+  posts.forEach(post => {
+    post.classList.remove('visible');
+
+    const postTags = post.getAttribute('data-tags').toLowerCase();
+    if (postTags.includes(searchTerm)) {
+      post.classList.add('visible');
+    }
+  });
+}
+
+
+const searchInput = document.getElementById('searchInput');
+searchInput.addEventListener('keypress', handleKeyPress);
+
+      
